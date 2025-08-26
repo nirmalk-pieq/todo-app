4ac3f0550c2efce668b936d96b55623a5f014484
Nirmalrajaa K <nirmal.k@pieq.ai>
2025-08-26 15:53:28 +0530

    Feat: Added login, logout functionality and added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,47 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+import Login from "./login";
+import Todo from "./todo";
 
 function App() {
-  // Load todos from localStorage initially
-  const [todos, setTodos] = useState<string[]>(() => {
-    const saved = localStorage.getItem("todos");
-    return saved ? JSON.parse(saved) : [];
-  });
 
-  const [newTodo, setNewTodo] = useState<string>("");
+  const [isLoggedIn, setIsLoggedIn] = useState(()=>{
+      return localStorage.getItem("isLoggedIn") === "true";
+  })
 
-  //updates the localstorage with the todos list when user gives input
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+  const handleLogin = ()=>{
+    setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn","true");
+  }
 
-  //addToDo validates the input and update in the list
-  const addTodo = () => {
-    if (newTodo.trim() === "") return;
-    setTodos([...todos, newTodo]);
-    setNewTodo("");
-  };
-
-  const deleteTodo = (valueRem: number) => {
-    setTodos((prevList) => prevList.filter((_, index) => index !== valueRem));
-  };
-
-  // _ has been used to indicate that the first parameter is not used in the filter function
+  const handleLogOut = ()=>{
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  }
 
   return (
-    <div>
-      <h1>Todo List</h1>
-      <p>Enter ToDo here!</p>
+    //router - Wraps everything and enables routing
+    <Router>
 
-      <input
-        placeholder="Add todo..."
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-      />
+      {/* Holds all the page definitions */}
+      <Routes>
 
-      <button onClick={addTodo}>+</button>
+        {/* Handles the single pages route address */}
+        <Route path="/login" 
+        element={
+          // calls the function isLoggedIn and check whether it is logged in.
+          // if not goes to handleLogin page 
+          isLoggedIn? <Navigate to ="/todos"/> : <Login onLogin={handleLogin}/>
+          } 
+        />
 
-      <h2>Todos:</h2>
-      <ul>
-        {todos.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => deleteTodo(index)}> - </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+        <Route path="/todos" 
+          element={
+            isLoggedIn? <Todo onLogout={handleLogOut}/> : <Navigate to = "/login"/>
+            }
+        />
+      </Routes>
+    </Router>
   );
 }
 
